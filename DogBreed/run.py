c27d038631500020970f32baee1a7ea2a7752243
camixxx <hatikoy@163.com>
2018-02-04 14:38:20 +0800

    switch to my account

--- DogBreed/run.py
+++ DogBreed/run.py
@@ -2,3 +2,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 18-2-4 下午2:31
 # @Author  : Camixxx
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import mxnet as mx
